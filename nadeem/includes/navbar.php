793b64cf6c41429b010475a1b4151b0326646da5
slash12 <mgardasaib@umail.utm.ac.mu>
2018-03-11 00:00:10 +0400

    need to finalize register and login, navbar and footer is ready

---
+++ nadeem/includes/navbar.php
@@ -0,0 +1,39 @@
+<!-- <script src="../js/jquery-3.2.1.slim.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+<script src="../js/bootstrap.min.js"></script> -->
+
+
+<?php
+
+?>
+<!-- <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"> -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navShirtPrint" aria-controls="navShirtPrint" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    <a class="navbar-brand" href="#">
+      <img src="img/logo_poule.png"  class="d-inline-block align-top" />
+      ShirtPrints
+    </a>
+    <div class="collapse navbar-collapse" id="navShirtPrint">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Brower T-Shirt</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Create T-Shirt</a>
+        </li>
+      </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li class="nav-item">
+                <a class="nav-link btn btn-default navbar-btn" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn btn-default navbar-btn" href="#">Register</a>
+            </li>
+        </ul>
+    </div>
+  </div>
+  </nav>
+  <!-- <script src="../js/bootstrap.min.js"></script> -->
---
+++ nadeem/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Index</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <?php
+      require("includes/navbar.php");
+    ?>
+
+    <?php
+      require("includes/footer.php");
+    ?>
+  </body>
+</html>
+<script src="js/jquery-3.2.1.slim.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
